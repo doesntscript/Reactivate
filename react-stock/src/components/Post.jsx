550bb80578efe75f2efcfb36a6262005ccc5c6c2
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-27 09:30:33 -0300

    (AuthPags): Outputting Dynamic Values

--- react-stock/src/components/Post.jsx
+++ react-stock/src/components/Post.jsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 
+const names = ['DevGui', 'GuiDeveloper'];
 
 
 const Post = () => {
+
+    const [ chosenName , setChosenName] = useState(names[0]);
+
+    useEffect (() => {
+        const interval = setInterval(() => { 
+        setChosenName( prevName => (prevName === names[0] ? names[1] : names[0]));
+    }, 10000); 
+
+    return () => clearInterval(interval);
+
+}, []);
+
   return (
     <>
-    <p> DevGui </p>
+    <p> {chosenName} </p>
     <p> React.Js is Awesome! </p>
     </>
 )
-}
+};
 
 export default Post;
