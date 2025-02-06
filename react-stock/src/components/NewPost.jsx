39b16efaf789db792ab7f198cee28994534979ad
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-06 17:28:39 -0300

    (AuthPags): Learning about Lifting State Up

--- react-stock/src/components/NewPost.jsx
+++ react-stock/src/components/NewPost.jsx
@@ -1,33 +1,33 @@
 
-import React, { useState } from 'react'
+
 
 import classes from './NewPost.module.css';
 
-export default function () {
+export default function (props) {
 
   // const stateData = useState("");
   // stateData[0] // current value / 
   // stateData[1] // state upddating funciton
 
-  const [ enteredBody, setEnteredBody ] = useState('');
+  // const [ enteredBody, setEnteredBody ] = useState('');
   // let enteredBody = '';
 
 
-  function ChangeTextHandler(event) {
-    setEnteredBody(event.target.value);
-  }
+  // function ChangeTextHandler(event) {
+  //   setEnteredBody(event.target.value);
+  // }
 
   return (
     <>
     <form className={classes.form}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id='body' required rows={3} onChange={ChangeTextHandler} />
+            <textarea id='body' required rows={3} onChange={props.onBodyChange} />
         </p>
-        <p> {enteredBody} </p>
+        
         <p>
             <label htmlFor='name'>Your name</label>
-            <input type='text' id='name' required />
+            <input type='text' id='name' required  onChange={props.onAuthorChange}/>
         </p>
     </form>
     </>
--- react-stock/src/components/PostsList.jsx
+++ react-stock/src/components/PostsList.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Post from './Post';
 import classes from './PostsList.module.css';
 import NewPost from './NewPost';
 
 export default function () {
+  const [ enteredBody, setEnteredBody ] = useState('');
+  const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+
+  }
 
   return (
     <>
-    <NewPost />
+    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
     <ul className={classes.posts}>
-        <Post author="GuiDeveloper" body="React.js is Awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="DevGui" body="Learning about ReactThreeFiber"/>
     </ul>
     </>
